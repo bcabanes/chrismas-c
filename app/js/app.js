9d41dd4a187fab9429f1089eb1a4eb88ed33e2cc
Ben <benjamin.cabanes@cossette.com>
2014-12-11 17:45:47 -0500

    add video in loop

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,10 @@
 (function($) {
     'use strict';
 
+    // Background video
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('/medias/snow.mp4', { ambient: true });
 
     // Init zoom function
     zoom.init();
