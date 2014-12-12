5a243d333d8bd82a29ddc97b6a8bac4dd7d01fe8
Ben <benjamin.cabanes@cossette.com>
2014-12-12 14:08:12 -0500

    update template

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,49 @@
-/* global jQuery, zoom */
+/* global window, navigator, jQuery, zoom */
 (function($) {
     'use strict';
 
 
-    // Detect IE
-    if(window.ActiveXObject || "ActiveXObject" in window){
-        // Always true if browser is Internet Explorer
-        document.write('Too bad! You\'re using Internet Explorer ... Pfff');
+    var experience = $('.experience'),
+        movie = $('.movie');
 
+    var isIe = detectIe(),
+        isUnderIos8 = isUnderIos8();
 
+    if(isIe || isUnderIos8){
+        experience.remove();
+        movie.show();
+        setMovieDimensions();
+        $(window).on('resize', setMovieDimensions);
 
-        
     }else{
         // Background video
-        var BV = new $.BigVideo({useFlashForFirefox:false});
+        var BV = new $.BigVideo({ useFlashForFirefox: false });
         BV.init();
         BV.show('/medias/snow.mp4', { ambient: true });
 
         // Init zoom function
         zoom.init();
+    }
+
+
+    function setMovieDimensions() {
+        'use strict';
+        var iframe = movie.children('iframe');
+        iframe.height($(window).height());
+        iframe.width($(window).width());
+    }
+
+    function detectIe() {
+        'use strict';
+        if(window.ActiveXObject || 'ActiveXObject' in window){ return true; }
+        else{ return false; }
+    }
+
+    function isUnderIos8() {
+        'use strict';
+        if(navigator.userAgent.match(/(iPad|iPhone|iPod touch);.*CPU.*OS 8_\d/i)){ return false; } // Is ios8
+        else if(navigator.userAgent.match(/(iPad|iPhone|iPod touch)/i)){ return true; } // Is uneder ios8
+        else{ return false; } // Is not ios
     }
 
 })(jQuery);
