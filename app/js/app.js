bd9122636d72cc1587fcccac8bf3ff6884f16fcf
Ben <benjamin.cabanes@cossette.com>
2014-12-11 19:08:10 -0500

    ie detection

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,24 @@
+/* global jQuery, zoom */
 (function($) {
     'use strict';
 
-    // Background video
-    var BV = new $.BigVideo();
-    BV.init();
-    BV.show('/medias/snow.mp4', { ambient: true });
 
-    // Init zoom function
-    zoom.init();
+    // Detect IE
+    if(window.ActiveXObject || "ActiveXObject" in window){
+        // Always true if browser is Internet Explorer
+        document.write('Too bad! You\'re using Internet Explorer ... Pfff');
+
+
+
+        
+    }else{
+        // Background video
+        var BV = new $.BigVideo({useFlashForFirefox:false});
+        BV.init();
+        BV.show('/medias/snow.mp4', { ambient: true });
+
+        // Init zoom function
+        zoom.init();
+    }
 
 })(jQuery);
